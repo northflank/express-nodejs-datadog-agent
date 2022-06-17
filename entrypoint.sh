0444144de3267c6365267872cb5bb339aa28e841
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2022-06-17 17:21:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 datadog-agent run &
-/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml &
-/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml &
+/opt/datadog-agent/embedded/bin/trace-agent &
+/opt/datadog-agent/embedded/bin/process-agent &
 yarn start 
