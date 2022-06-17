57ad3f47302a60142f2dfabe3df3f9d1726dbe7e
Champ-Goblem <cameron_mcdermott@yahoo.co.uk>
2022-06-17 18:14:05 +0100

    update the logs piping

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-datadog-agent run &1> /var/log/agent.log &2> /var/log/agent.log &
-/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml &1> /var/log/trace-agent.log &2> /var/log/trace-agent.log &
-/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml &1> /var/log/process-agent.log &2> /var/log/process-agent.log &
-yarn start &1> /var/log/app.log &2> /var/log/app.log &
+datadog-agent run 1>/var/log/agent.log 2>/var/log/agent.log &
+/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml 1> /var/log/trace-agent.log 2> /var/log/trace-agent.log &
+/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml 1> /var/log/process-agent.log 2> /var/log/process-agent.log &
+yarn start 1> /var/log/app.log 2> /var/log/app.log &
 
 tail -f /var/log/agent.log &
 tail -f /var/log/trace-agent.log &
